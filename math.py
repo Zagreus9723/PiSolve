4928eb281df04610a0d7a78085eb546ef241110d
Apophis <61201959+Apophis52@users.noreply.github.com>
2024-02-20 21:35:38 -0600

    found api

--- math.py
+++ math.py
@@ -2,20 +2,23 @@
 #
 from PIL import Image
 from pix2tex.cli import LatexOCR
-from sympy.parsing.latex import parse_latex
-from sympy.abc import x
-from sympy import solve, solveset
 from io import StringIO
 import sys
-from sympy import init_printing
+import requests
+import json
+import sympy
 import math
+import numpy
+import numpy as np
 
 rads = {
-    "0.87": "sqrt(3)/2",
-    "0.71": "sqrt(2)/2",
-    "0.5": "1/2",
-    "1.0": "1"
+    ".87": "sqrt(3)/2",
+    ".71": "sqrt(2)/2",
+    ".5": "1/2",
+    "1": "1"
 }
+
+reds = ['.87', '.71', '.5']
 
 def unitCircTrig(sol):
     old_stdout = sys.stdout
@@ -25,14 +28,44 @@ def unitCircTrig(sol):
     message = mystdout.getvalue().replace("\n", "")
     return message
 
-img = Image.open('lagrida_latex_editor.png')
+img = Image.open('Screenshot 2024-02-20 213209.png')
 model = LatexOCR()
 ltx = model(img).replace("\ ", "")
 print(ltx)
+
+code = requests.get('https://api.computegpt.org/?q=' + "Solve. " + ltx).content
+print(code.decode())
+old_stdout = sys.stdout
+sys.stdout = mystdout = StringIO()
+exec(code)
+sys.stdout = old_stdout
+message = mystdout.getvalue().replace("\n", "")
+
+try:
+    if round(int(message), 2) in rads:
+        message = f"{str(message)},,,{str(unitCircTrig(message))},,,{str(rads[str(round(float(unitCircTrig(message)), 2))])}"
+except:
+    pass
+
+print(message)
+
+
+
+
+
+
+
+#-----------------------------------------
+# from sympy import init_printing
+# import math
+# from sympy.parsing.latex import parse_latex
+# from sympy.abc import x
+# from sympy import solve, solveset
+
 # ltx = """3x^{7}+36x^{5}+108x^{3}"""
-equation = parse_latex(ltx)
-sol = solve(equation, x)
-for x in range(len(sol)):
-    if 'pi' in str(sol[x]):
-        sol[x] = f"{str(sol[x])},,,{str(unitCircTrig(sol[x]))},,,{str(rads[str(round(float(unitCircTrig(sol[x])), 2))])}"
-print(sol)
+# equation = parse_latex(ltx)
+# sol = solve(equation, x)
+# for x in range(len(sol)):
+#     if 'pi' in str(sol[x]):
+#         sol[x] = f"{str(sol[x])},,,{str(unitCircTrig(sol[x]))},,,{str(rads[str(round(float(unitCircTrig(sol[x])), 2))])}"
+# print(sol)
